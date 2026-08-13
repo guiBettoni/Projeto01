5b7d420c83d4065e556718ae04e213a46cb2f4ee
Guilherme Bettoni <guilherme.pavao99@gmail.com>
2026-08-13 10:21:01 -0300

    ajustes

--- src/main/java/com/example/Projeto01/models/User.java
+++ src/main/java/com/example/Projeto01/models/User.java
@@ -24,13 +24,12 @@ public class User {
   public interface UpdateUser {
   }
 
-  public static final String 
- = "user";
-   
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true)
-    private Long id;
+  public static final String TABLE_NAME = "user";
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", unique = true)
+  private Long id;
 
   @Column(name = "username", length = 100, nullable = false, unique = true)
   @NotNull(groups = CreateUser.class)
